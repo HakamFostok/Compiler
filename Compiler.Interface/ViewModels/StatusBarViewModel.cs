3cd1f377bc2dfa0bf1f03d6bc2c7d2df0adf59ad
Hakan FISTIK <hakam.fstk@gmail.com>
2021-03-07 11:06:01 +0300

    Add new Files

---
+++ Compiler.Interface/ViewModels/StatusBarViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler.Interface.ViewModels
+{
+    public class StatusBarViewModel : BaseViewModel
+    {
+        private CompilerStatus status;
+        public CompilerStatus Status
+        {
+            get => status;
+            set
+            {
+                SetProperty(ref status, value);
+                RaisePropertyChanged(nameof(StatusDescription));
+            }
+        }
+
+        public string StatusDescription => Status.ToString();
+
+        public StatusBarViewModel()
+        {
+            Status = CompilerStatus.Ready;
+        }
+    }
+}
